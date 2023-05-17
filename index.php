29fd38d698db9b2be505072daafa61049684ed39
0roc4n <76811555+0roc4n@users.noreply.github.com>
2023-05-17 13:55:44 +0800

    link property listing

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $user_data = check_login($con);
         </div>
         <ul class="nav-links">
             <li><a href="#">Home</a></li>
-            <li><a href="#">Properties</a></li>
+            <li><a href="property-listing.php">Properties</a></li>
             <li><a href="#">Buyer/seller</a></li>
             <li><a href="#">Contact</a></li>
             <li><a href="#">About us</a></li>
