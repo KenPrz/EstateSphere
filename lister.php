2cd6d85c5e2eb8971071965d9b00ab9ec6f91193
-Haru- <52265509+KenPrz@users.noreply.github.com>
2023-05-22 16:58:33 +0800

    updated paginations and swipers

--- lister.php
+++ lister.php
@@ -4,25 +4,22 @@ include("connection.php");
 include("functions.php");
 $user_data = check_login($con);
 ?>
-
 <!DOCTYPE html>
 <html>
-
 <head>
-    <title>Home</title>
+    <title>For Sale</title>
     <link rel="stylesheet" href="./css/home.css">
     <link rel="stylesheet" href="./css/navbar.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
 </head>
-
 <body>
-    <nav>
+<nav>
         <div class="logo">
             <a href="index.php">
                 <img src="./assets/img/logo.svg" alt="Logo">
-                <a href="#">Estatesphere</a>
+                <a href="index.php">Estatesphere</a>
             </a>
         </div>
         <ul class="nav-links">
@@ -41,7 +38,7 @@ $user_data = check_login($con);
             </select>
         </div>
     </nav>
-    <div class="header d-flex align-items-center">
+    <div class="property-header d-flex align-items-center">
         <div class="row ms-4">
             <div class="col-auto">
                 <!-- Searchbar -->
@@ -101,176 +98,49 @@ $user_data = check_login($con);
             </div>
         </div>
     </div>
-    <main>
-        <div class="row">
-            <div class="title-header">
-                <h6 class="m-5">Buy Properties Listed in <b>Albay</b></h6>
+<div class="container mt-5">
+    <!-- Carousel -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+<div #swiperRef="" class="swiper mySwiper">
+    <div class="swiper-wrapper">
+        <?php
+            $query = "SELECT property_municipality, COUNT(*) AS count FROM property GROUP BY property_municipality;";
+            $result = mysqli_query($con, $query);
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                $municipality = $row['property_municipality'];
+                $count = $row['count'];
+        ?>
+        <div class="swiper-slide" style="width:357px; height:144px;">
+            <div class="row">
+                <div class="row">
+                    <h3><?php echo $municipality; ?></h3>
+                </div>
+                <div class="row">
+                    <div class="col-auto">
+                        <img src="./assets/img/house-solid.svg" style="height: 30px; width: 30px;" alt="">
+                    </div>
+                    <div class="col">
+                        <p class="mb-0 mt-1"><?php echo $count; ?> properties for sale</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <a href="#" style="text-decoration: none; font-size: 14px;" class="text-primary mt-2">view listed properties</a>
+                </div>
             </div>
         </div>
-        <div class="container">
+        <?php
+            }
+            mysqli_free_result($result);
+        ?>
+    </div>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+    <!-- <div class="swiper-pagination"></div> -->
+</div>
             <!-- Carousel -->
-            <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
-            <div #swiperRef="" class="swiper mySwiper">
-                <div class="swiper-wrapper">
-                    <?php
-                    $query = "SELECT property_municipality, COUNT(*) AS count FROM property GROUP BY property_municipality;";
-                    $result = mysqli_query($con, $query);
-
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        $municipality = $row['property_municipality'];
-                        $count = $row['count'];
-                        ?>
-                        <div class="swiper-slide" style="width:357px; height:144px;">
-                            <div class="row">
-                                <div class="row">
-                                    <h3>
-                                        <?php echo $municipality; ?>
-                                    </h3>
-                                </div>
-                                <div class="row">
-                                    <div class="col-auto">
-                                        <img src="./assets/img/house-solid.svg" style="height: 30px; width: 30px;" alt="">
-                                    </div>
-                                    <div class="col">
-                                        <p class="mb-0 mt-1">
-                                            <?php echo $count; ?> properties for sale
-                                        </p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <a href="#" style="text-decoration: none; font-size: 14px;"
-                                        class="text-primary mt-2">view listed properties</a>
-                                </div>
-                            </div>
-                        </div>
-                        <?php
-                    }
-                    mysqli_free_result($result);
-                    ?>
-                </div>
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
-                <!-- <div class="swiper-pagination"></div> -->
-            </div>
-            <!-- Carousel -->
-            <div class="row my-3">
-                <div class="col-6">
-                    <h4 class="float-left" style="font-weight: 900;">Featured Properties For <b> Sale </b></h4>
-                </div>
-                <div class="col-6">
-                    <a href="featured.php" style="text-decoration: none;" class="float-end .text-primary pe-5">See more
-                        ></a>
-                </div>
-                <!-- Card Rows -->
-                <div class="row d-flex justify-content-center p-4" style="background-color: white;">
-                    <!-- CARD -->
-                    <?php
-                    // Assuming you have established a database connection and stored it in the variable $con
-                    
-                    // Fetch the first 5 rows from the "properties" table
-                    $query = "SELECT * FROM property LIMIT 5";
-                    $result = mysqli_query($con, $query);
-
-                    // Check if the query was successful
-                    if ($result) {
-                        // Iterate over the fetched rows and populate the cards
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            $property_name = $row['property_name'];
-                            $location = $row['property_municipality'];
-                            $price = $row['property_price'];
-                            $img = $row['property_img_addrs'];
-                            $num_of_beds = $row['num_of_beds'];
-                            $num_of__baths = $row['num_of_baths'];
-                            $num_of_carports = $row['num_of_carports'];
-
-                            // Generate the HTML card with the fetched data
-                            ?>
-                            <div class="card mx-2" style="width: 14rem; background-color: #F9F9F9; color: black;">
-                                <img src="<?php echo $img; ?>" class="card-img mt-2" alt="image"
-                                    style="width: 100%; height: 100%; object-fit: cover;">
-                                <div class="card-body px-0">
-                                    <h6 class="card-subtitle mb-1" style="font-size:10px">
-                                        <img src="./assets/img/icons/location.png" alt=""
-                                            style="width: 13px; height: 13px; margin-right:5px; object-fit: cover;">
-                                        <?php echo $location; ?>
-                                    </h6>
-                                    <h5 class="card-title mb-" style="font-size: 18px; font-family: Montserrat, sans-serif;">
-                                        <span class="highlight-text">
-                                            <?php echo $property_name; ?>
-                                        </span>
-                                    </h5>
-                                    <h6 class="card-subtitle"
-                                        style="color: #18A0FB; font-size: 18px; font-family: Montserrat, sans-serif;">
-                                        ₱<b>
-                                            <?php echo number_format($price, 0, ',', ','); ?>
-                                        </b>
-                                    </h6>
-
-                                    <div class="card-text">
-                                        <div class="row justify-content-center">
-                                            <h4 class="border-bottom">
-                                                <?php
-                                                $stars = rand(2, 5); // Generate a random number between 2 and 5
-                                                for ($i = 1; $i <= $stars; $i++) {
-                                                    ?>
-                                                    <img src="./assets/img/icons/star.png" style="height: 15px; width: 15px;">
-                                                    <?php
-                                                }
-                                                ?>
-                                            </h4>
-                                            <div class="row justify-content-center my-2">
-                                                <div class="col-4">
-                                                    <div class="d-flex align-items-center justify-content-center">
-                                                        <span class="me-1">
-                                                            <?php echo $num_of_beds; ?>
-                                                        </span>
-                                                        <img src="./assets/img/icons/bed.png" alt=""
-                                                            style="width: 25px; height: 25px;">
-                                                    </div>
-                                                    <div class="row">
-                                                        <p style="font-size:8px">Bedrooms</p>
-                                                    </div>
-                                                </div>
-                                                <div class="col-4">
-                                                    <div class="d-flex align-items-center justify-content-center">
-                                                        <span class="me-1">
-                                                            <?php echo $num_of__baths; ?>
-                                                        </span>
-                                                        <img src="./assets/img/icons/bathtub.png" alt=""
-                                                            style="width: 25px; height: 25px;">
-                                                    </div>
-                                                    <div class="row">
-                                                        <p style="font-size:8px">Bathroom</p>
-                                                    </div>
-                                                </div>
-                                                <div class="col-4">
-                                                    <div class="d-flex align-items-center justify-content-center">
-                                                        <span class="me-1">
-                                                            <?php echo $num_of_carports; ?>
-                                                        </span>
-                                                        <img src="./assets/img/icons/garage.png" alt=""
-                                                            style="width: 25px; height: 25px;">
-                                                    </div>
-                                                    <div class="row">
-                                                        <p style="font-size:8px">Carports</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <button type="button" class="btn btn-primary btn-sm btn-block w-100">view more</button>
-                                    </div>
-                                </div>
-                            </div>
-                            <?php
-                        }
-                    } else {
-                        // Handle the case when the query fails
-                        echo "Failed to fetch data from the database.";
-                    }
-                    ?>
-                    <!-- CARD -->
-                </div>
-                <!-- Card Rows -->
+</div>          
+            <div class="container">
                 <div class="row my-3">
                     <div class="col-6">
                         <h4 class="float-left" style="font-weight: 900;">Featured Properties For Rent</h4>
@@ -391,10 +261,7 @@ $user_data = check_login($con);
                     <!-- Card Rows -->
                 </div>
             </div>
-    </main>
 </body>
-
-</html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
     crossorigin="anonymous"></script>
